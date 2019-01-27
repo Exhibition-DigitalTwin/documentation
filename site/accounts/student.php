6fc0dc414f8e4312e0551b948641f5f6e6d3f7e1
Ana <ana.keser@web.de>
2019-01-27 12:10:27 +0100

    Zwischenspeichern: Ideensammlung fast fertig

--- site/accounts/student.php
+++ site/accounts/student.php
@@ -9,4 +9,9 @@ firstname: Team-DT
 lastname: ""
 email: ""
 history:
-  - der-digitale-zwilling1
+  - >
+    der-digitale-zwilling/ideation/ideensammlung-brainstorming-sketching
+  - der-digitale-zwilling/ideation/kommunikation-deskriptiv-szenographisch-kontextualisierend
+  - der-digitale-zwilling/ideation/konzept-1-create-your-own-lego-figure
+  - der-digitale-zwilling/ideation
+  - der-digitale-zwilling
